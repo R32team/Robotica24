7504019c9960612749c328e3c3772e6d88a7f11c
Haseeb Nabi <162741703+Has33b-Nabi@users.noreply.github.com>
2024-04-30 17:39:12 +0200

    coding

--- coding/base_info_calend 2/face tracker.pml
+++ coding/base_info_calend 2/face tracker.pml
@@ -7,14 +7,12 @@
     <Dialogs>
         <Dialog name="ExampleDialog" src="behavior_1/ExampleDialog/ExampleDialog.dlg" />
         <Dialog name="variabili" src="variabili/variabili.dlg" />
-        <Dialog name="d1" src="d1/d1.dlg" />
         <Dialog name="domanda" src="domanda/domanda.dlg" />
     </Dialogs>
     <Resources />
     <Topics>
         <Topic name="ExampleDialog_enu" src="behavior_1/ExampleDialog/ExampleDialog_enu.top" topicName="ExampleDialog" language="en_US" nuance="enu" />
         <Topic name="variabili_iti" src="variabili/variabili_iti.top" topicName="variabili" language="it_IT" nuance="iti" />
-        <Topic name="d1_iti" src="d1/d1_iti.top" topicName="d1" language="it_IT" nuance="iti" />
         <Topic name="domanda_iti" src="domanda/domanda_iti.top" topicName="domanda" language="it_IT" nuance="iti" />
     </Topics>
     <IgnoredPaths />
--- coding/base_info_calend 2/translations/translation_it_IT.ts
+++ coding/base_info_calend 2/translations/translation_it_IT.ts
@@ -2,30 +2,7 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="it_IT">
     <context>
-        <name>behavior_1/behavior.xar:/Animated Say</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>scusi, non riesco più a vederla, può venire davanti a mè,   grazie</source>
-            <comment>Text</comment>
-            <translation type="unfinished">scusi, non riesco più a vederla, può venire davanti a mè,   grazie</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
-        <message>
-            <source>Ciao</source>
-            <comment>Text</comment>
-            <translation type="vanished">Ciao</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>metta i documenti sul nao car,  poi dica&quot;invia la car&quot; </source>
-            <comment>Text</comment>
-            <translation type="unfinished">metta i documenti sul nao car,  poi dica&quot;invia la car&quot; </translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Choice (1)</name>
+        <name>behavior_1/behavior.xar:/Choice</name>
         <message>
             <source>no/no NAO/non proprio/non credo</source>
             <comment>SpeechReco negative</comment>
@@ -90,87 +67,6 @@
             <source>C’è troppo rumore qui! /Prova ad articolare meglio le parole. /Non ti sento bene. </source>
             <comment>TTS notUnderstoodAnims</comment>
             <translation type="vanished">C’è troppo rumore qui! /Prova ad articolare meglio le parole. /Non ti sento bene. </translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Choice (1)/Choice</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>no/no nao/disagree/I disagree</source>
-            <comment>SpeechReco negative</comment>
-            <translation type="unfinished">no/no nao/disagree/I disagree</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>yes/yes nao/agree/I agree/of course</source>
-            <comment>SpeechReco positive</comment>
-            <translation type="unfinished">yes/yes nao/agree/I agree/of course</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>help/help me/i don't know</source>
-            <comment>SpeechReco help</comment>
-            <translation type="unfinished">help/help me/i don't know</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>exit/stop/quit</source>
-            <comment>SpeechReco exit</comment>
-            <translation type="unfinished">exit/stop/quit</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>repeat/pardon/what/excuse me</source>
-            <comment>SpeechReco repeat</comment>
-            <translation type="unfinished">repeat/pardon/what/excuse me</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>I understood %s. Is that correct?</source>
-            <comment>TTS confirmation</comment>
-            <translation type="unfinished">I understood %s. Is that correct?</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>, / or </source>
-            <comment>TTS enumMarks</comment>
-            <translation type="unfinished">, / or </translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
-            <comment>TTS helpEnumChoices</comment>
-            <translation type="unfinished">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source> you can ask me: %s. </source>
-            <comment>TTS helpEnumDefault</comment>
-            <translation type="unfinished"> you can ask me: %s. </translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
-            <comment>TTS helpTactile</comment>
-            <translation type="unfinished">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>I did not understand. </source>
-            <comment>TTS notUnderstood</comment>
-            <translation type="unfinished">I did not understand. </translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>No question has been defined, so I cannot repeat it. </source>
-            <comment>TTS noQuestion</comment>
-            <translation type="unfinished">No question has been defined, so I cannot repeat it. </translation>
-        </message>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
-            <comment>TTS notUnderstoodAnims</comment>
-            <translation type="unfinished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
         </message>
     </context>
     <context>
@@ -252,24 +148,6 @@
             <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
             <comment>TTS notUnderstoodAnims</comment>
             <translation type="unfinished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Say</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>dica nome, cognome, data e orario nello stesso ordine</source>
-            <comment>Text</comment>
-            <translation type="unfinished">dica nome, cognome, data e orario nello stesso ordine</translation>
-        </message>
-    </context>
-    <context>
-        <name>behavior_1/behavior.xar:/Say (1)</name>
-        <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
-            <source>mi dica la diomanda a cui devo ripsondere</source>
-            <comment>Text</comment>
-            <translation type="unfinished">mi dica la diomanda a cui devo ripsondere</translation>
         </message>
     </context>
 </TS>
